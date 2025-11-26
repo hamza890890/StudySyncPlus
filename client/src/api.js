8027cee049b27d32761481ecf822510ab946b1b2
hamza890890 <h_ahmed2@uncg.edu>
2025-11-26 17:45:06 -0500

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+import axios from "axios";
+
 const API = axios.create({
- // baseURL: "https://studysyncplus-backend.onrender.com/api"
-  baseURL: import.meta.env.VITE_API_BASE_URL
-
-
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api"
 });
 
 export default API;
+
+
+
