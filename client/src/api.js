ed3b3c66e7498e3af1708359d135cb0db6fd3a4c
hamza890890 <h_ahmed2@uncg.edu>
2025-11-26 18:02:36 -0500

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-import axios from "axios";
-
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 export default API;
-
-
-
