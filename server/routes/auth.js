631f700861c219a2c211968a9d89de3cafacc9a9
hamza890890 <h_ahmed2@uncg.edu>
2025-11-26 11:14:57 -0500

    Made changes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
